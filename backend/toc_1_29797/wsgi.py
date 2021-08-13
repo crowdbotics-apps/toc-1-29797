01d9dfdd13633eb38c847190f7b6774368724ded
Crowdbotics <team@crowdbotics.com>
2021-08-13 23:07:56 +0000

    Initial commit.

---
+++ backend/toc_1_29797/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for toc_1_29797 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toc_1_29797.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "toc_1_29797Identifier",
+  url: "https://toc_1_29797Identifier.botics.co",
+  api: "https://toc_1_29797Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
